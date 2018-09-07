429568b99758582e3c76c77a7874567483e98868
root <root@DESKTOP-8NN91NC.localdomain>
2018-09-07 08:40:09 +0200

    code refactor

--- Main.py
+++ Main.py
@@ -5,8 +5,55 @@ SIZE_OUT = 3;
 SIZE_INT = 2;
 
 def sigmoid(x, derivative=False):
-  return x*(1-x) if derivative else 1/(1+np.exp(-x))
+  return x * (1 - x) if derivative else 1 / (1 + np.exp(-x))
 
+class NeuronLayer :
+    def __init__(this, size, activation = sigmoid):        
+        this.set(size, activation): 
+
+    def activate(this):
+        this.activation(this.vector);
+
+    def set(this, size, activation = sigmoid): 
+        this.size = size;
+        this.activation = activation;
+        this.vector = np.zeros(size);
+   
+   def reinit(this):
+       this.vector = np.zeros(size);
+
+class NeuralNetwork :
+    def __init__(this, shape, activation = sigmoid, values = None):
+        this.coefs = [];
+        this.layers = [];
+        if values == None:
+            this.values = [];
+            for i in range(len(shape) - 1):
+                this.values = this.values + [0.0] * shape[i] * shape[i + 1];
+        this.values = np.array(this.values);
+        this.putValuesInCoefs();
+        for i in range(len(shape)):
+            this.layers.append(NeuronLayer(shape[i], activation)); 
+
+    def putValuesInCoefs(this, values = None):
+        if values != None:
+            this.values = values;
+        this.coefs = []
+        j = 0;
+        for i in range(len(this.shape) - 1):
+            matrix = np.zeros(this.shape[i], this.shape[i + 1]);
+            for x in range(this.shape[i]):
+                for y in range(this.shape[i]):
+                    matrix[x,y] = this.values[j];
+                    j++;
+            this.coefs.append(matrix);
+    
+    def feedInput(this, values):
+        if (len(values) == len(this.layers[0].size)):
+            for i in range(len(values)):
+                this.layers[0].vector[i] = values[i];
+            
+"""
 class NeuralNetwork :
     def __init__(this, weightMatrix = None):
         this.vectorIn = np.mat(1, SIZE_IN + 1);
@@ -34,4 +81,4 @@ class NeuralNetwork :
                 this.neuralMatrix[k] = np.matrix
 
             this.vectorOut = this.weightMatrix[-1] * this.neuralMatrix[-1]
-
+"""
